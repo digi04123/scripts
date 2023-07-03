8b891b15e81a7b41363bd4f6f13a178f045b43d5
digi04123 <digi04123@gmail.com>
2023-07-03 12:14:11 +0800

    update script

---
+++ initial_setup_config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Check out https://www.atlassian.com/git/tutorials/dotfiles for more details
+git init --bare $HOME/.cfg
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+config config --local status.showUntrackedFiles no
+echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.bashrc
--- setup_config.sh
+++ setup_config.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-git init --bare $HOME/.cfg
+# Check out https://www.atlassian.com/git/tutorials/dotfiles for more details
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+echo ".cfg" >> .gitignore
+git clone --bare https://github.com/digi04123/config $HOME/.cfg
+rm -f ~/.zshrc
+config checkout
 config config --local status.showUntrackedFiles no
-echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.bashrc
